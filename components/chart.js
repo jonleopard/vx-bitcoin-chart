de9cb63af662b108cc3ab4a137601531eaab6de8
Jon Leopard <jonlprd@gmail.com>
2018-02-05 16:34:01 +0100

    Component refactor

--- components/chart.js
+++ components/chart.js
@@ -1,22 +1,197 @@
 import { withParentSize } from '@vx/responsive';
+import { scaleTime, scaleLinear } from '@vx/scale';
+import { LinePath, AreaClosed, Bar, Line } from '@vx/shape';
+import { LinearGradient } from '@vx/gradient';
+import { AxisBottom } from '@vx/axis';
+import { withTooltip, Tooltip } from '@vx/tooltip';
+import { localPoint } from '@vx/event';
+import { bisector } from 'd3-array';
+import formatDate from '../utils/formatDate';
 
-function Chart({ data, parentWidth, parentHeight }) {
-  const margin = {
-    top: 15,
-    bottom: 40,
-    left: 0,
-    right: 0,
-  };
-  const width = parentWidth - margin.left - margin.right;
-  const height = parentHeight - margin.top - margin.bottom;
+import formatPrice from '../utils/formatPrice';
+import MaxPrice from './maxprice';
+import MinPrice from './minprice';
 
-  return (
-    <div>
-      <svg width={width} height={height}>
-        <rect width={width} height={height} fill="steelblue" />
-      </svg>
-    </div>
-  );
+class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const {
+      data,
+      parentWidth,
+      parentHeight,
+      tooltipLeft,
+      tooltipTop,
+      tooltipData,
+      showTooltip,
+      hideTooltip,
+    } = this.props;
+
+    const margin = {
+      top: 15,
+      bottom: 40,
+      left: 0,
+      right: 0,
+    };
+
+    const width = parentWidth - margin.left - margin.right;
+    const height = parentHeight - margin.top - margin.bottom;
+
+    const bisectDate = bisector(d => x(d)).left;
+
+    const x = d => new Date(d.time);
+    const y = d => d.price;
+    const firstPoint = data[0];
+    const currentPoint = data[data.length - 1];
+
+    const minPrice = Math.min(...data.map(y));
+    const maxPrice = Math.max(...data.map(y));
+    const maxPriceData = [
+      { time: x(firstPoint), price: maxPrice },
+      { time: x(currentPoint), price: maxPrice },
+    ];
+    const minPriceData = [
+      { time: x(firstPoint), price: minPrice },
+      { time: x(currentPoint), price: minPrice },
+    ];
+    const xScale = scaleTime({
+      range: [0, width],
+      domain: [Math.min(...data.map(x)), Math.max(...data.map(x))],
+    });
+
+    const yScale = scaleLinear({
+      range: [height, 0],
+      domain: [minPrice, maxPrice],
+    });
+
+    return (
+      <div>
+        <svg ref={s => (this.svg = s)} width={width} height={parentHeight}>
+          <AxisBottom
+            top={yScale(minPrice)}
+            data={data}
+            scale={xScale}
+            x={x}
+            numTicks={4}
+            hideAxisLine
+            hideTicks
+            tickLabelComponent={<text fill="#ffffff" fontSize={11} />}
+          />
+          <LinearGradient
+            id="area-fill"
+            from="#4682b4"
+            to="#4682b4"
+            fromOpacity={0.3}
+            toOpacity={0}
+          />
+          <MaxPrice
+            data={maxPriceData}
+            yScale={yScale}
+            xScale={xScale}
+            x={x}
+            y={y}
+            label={formatPrice(maxPrice)}
+            yText={yScale(maxPrice)}
+          />
+
+          <MinPrice
+            data={minPriceData}
+            yScale={yScale}
+            xScale={xScale}
+            x={x}
+            y={y}
+            label={formatPrice(minPrice)}
+            yText={yScale(minPrice)}
+          />
+          <AreaClosed
+            data={data}
+            yScale={yScale}
+            xScale={xScale}
+            x={x}
+            y={y}
+            fill="url(#area-fill)"
+            stroke="transparent"
+          />
+          <LinePath data={data} yScale={yScale} xScale={xScale} x={x} y={y} />
+          <Bar
+            data={data}
+            width={width}
+            height={height}
+            fill="transparent"
+            onMouseLeave={data => event => hideTooltip()}
+            onMouseMove={data => (event) => {
+              const { x: xPoint } = localPoint(this.svg, event);
+              const x0 = xScale.invert(xPoint);
+              const index = bisectDate(data, x0, 1);
+              const d0 = data[index - 1];
+              const d1 = data[index];
+              const d = x0 - xScale(x(d0)) > xScale(d(d1)) - x0 ? d1 : d0;
+              showTooltip({
+                tooltipLeft: xScale(x(d)),
+                tooltipTop: yScale(y(d)),
+                tooltipData: d,
+              });
+            }}
+          />
+          {tooltipData && (
+            <g>
+              <Line
+                from={{
+                  x: tooltipLeft,
+                  y: yScale(y(maxPriceData[0])),
+                }}
+                to={{
+                  x: tooltipLeft,
+                  y: yScale(y(minPriceData[0])),
+                }}
+                stroke="#ffffff"
+                strokeDasharray="2,2"
+              />
+              <circle
+                r="8"
+                cx={tooltipLeft}
+                cy={tootipTop}
+                fill="#00f1a1"
+                fillOpacity={0.4}
+                style={{ pointerEvents: 'none' }}
+              />
+              <circle
+                r="4"
+                cx={tooltipLeft}
+                cy={tootipTop}
+                fill="#00f1a1"
+                style={{ pointerEvents: 'none' }}
+              />
+            </g>
+          )}
+        </svg>
+        {tooltipData && (
+          <div>
+            <Tooltip
+              top={tooltipTop - 12}
+              left={tooltipLeft + 12}
+              style={{
+                backgroundColor: '#6086d6',
+                color: '#ffffff',
+              }}
+            >
+              {formatPrice(y(tooltipData))}
+            </Tooltip>
+            <Tooltip
+              left={tooltipLeft}
+              top={yScale(minPrice)}
+              style={{
+                transform: 'translate(-50%)',
+              }}
+            >
+              {formatDate(x(tooltipData))}
+            </Tooltip>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
-export default withParentSize(Chart);
+export default withParentSize(withTooltip(Chart));
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,6 @@
 import { withScreenSize } from '@vx/responsive';
-import { LinearGradient } from '@vx/gradient';
-import Chart from '../components/chart';
-
-function Background({ width, height }) {
-  return (
-    <svg width={width} height={height}>
-      <LinearGradient id="fill" vertical={false}>
-        <stop stopColor="#a943e4" offset="0%" />
-        <stop stopColor="#f55989" offset="50%" />
-        <stop stopColor="#ffaf84" offset="100%" />
-      </LinearGradient>
-      <rect width={width} height={height} fill="steelblue" fill="url(#fill)" />
-    </svg>
-  );
-}
+import Background from '../components/background';
+import BitcoinPrice from '../components/bitcoinprice';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,7 +9,6 @@ class App extends React.Component {
       data: {},
     };
   }
-
   componentDidMount() {
     fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
       .then(res => res.json())
@@ -35,80 +21,37 @@ class App extends React.Component {
   render() {
     const { screenWidth, screenHeight } = this.props;
     const { data } = this.state;
-    if (!data.bpi) return <div>Loading...</div>;
-    const prices = Object.keys(data.bpi).map(k => ({
-      time: k,
-      price: data.bpi[k],
-    }));
-    const currentPrice = prices[prices.length - 1].price;
     return (
       <div className="app">
         <Background width={screenWidth} height={screenHeight} />
         <div className="center">
-          <div className="chart">
-            <div className="titlebar">
-              <div className="title">
-                <div>Bitcoin Price</div>
-                <div>
-                  <small>last 30 days</small>
-                </div>
-              </div>
-              <div className="spacer" />
-              <div>
-                <div>{currentPrice}</div>
-              </div>
-            </div>
-            <div className="container">
-              <Chart data={data} />
-            </div>
-          </div>
+          <BitcoinPrice data={data} width={screenWidth} height={screenHeight} />
           <p className="disclaimer">{data.disclaimer}</p>
         </div>
-        <style jsx>
-          {`
-            .app,
-            .center {
-              display: flex;
-              position: absolute;
-              top: 0;
-              left: 0;
-              right: 0;
-              bottom: 0;
-              flex: 1;
-              justify-content: center;
-              align-items: center;
-              font-family: arial;
-              flex-direction: column;
-            }
-            .title {
-            }
-            .titlebar {
-              display: flex;
-              flex-direction: row;
-              align-items: center;
-              padding: 15px;
-            }
-            .spacer {
-              flex: 1;
-            }
-            .container {
-              flex: 1,
-              display: flex;
-            }
-            .chart {
-              width: 600px;
-              height: 400px;
-              background-color: #27273f;
-              border-radius: 8px;
-              color: white;
-              flex-direction: column;
-            }
-            .disclaimer {
-              color: white;
-              opacity: 0.4;
-              font-size: 12px;
-            }
-          `}
+        <style jsx>{`
+          .app,
+          .center {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            overflow: hidden;
+            display: flex;
+            font-family: arial;
+            flex-direction: column;
+          }
+          .disclaimer {
+            margin-top: 35px;
+            font-size: 11px;
+            color: white;
+            opacity: 0.4;
+          }
+          .center {
+            align-items: center;
+            justify-content: center;
+          }
+        `}
         </style>
       </div>
     );
